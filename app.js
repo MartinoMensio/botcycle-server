e05369b256346b8273839d266f568acb591de421
Martino Mensio <martinomensio@outlook.it>
2017-04-17 12:14:28 +0200

    setting up botkit on botframework

---
+++ app.js
@@ -0,0 +1,35 @@
+var Botkit = require('botkit');
+var controller = Botkit.botframeworkbot({
+});
+
+var bot = controller.spawn({
+        appId: process.env.app_id,
+        appPassword: process.env.app_password
+});
+
+// if you are already using Express, you can use your own server instance...
+// see "Use BotKit with an Express web server"
+controller.setupWebserver(process.env.port,function(err,webserver) {
+  controller.createWebhookEndpoints(controller.webserver, bot, function() {
+      console.log('This bot is online!!!');
+  });
+});
+
+// user said hello
+controller.hears(['hello'], 'message_received', function(bot, message) {
+
+    bot.reply(message, 'Hey there.');
+
+});
+
+controller.hears(['cookies'], 'message_received', function(bot, message) {
+
+    bot.startConversation(message, function(err, convo) {
+
+        convo.say('Did someone say cookies!?!!');
+        convo.ask('What is your favorite type of cookie?', function(response, convo) {
+            convo.say('Golly, I love ' + response.text + ' too!!!');
+            convo.next();
+        });
+    });
+});

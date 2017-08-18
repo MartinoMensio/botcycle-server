510d564b45fca1fdbd50366007a48b719c39f751
Martino Mensio <martinomensio@outlook.it>
2017-08-18 16:52:27 +0200

    adding websocket for communication with brain

--- app.js
+++ app.js
@@ -1,33 +1,90 @@
+const express = require('express')
+const http = require('http')
+const url = require('url')
+const WebSocket = require('ws')
+const path = require('path')
+const bodyParser = require('body-parser')
 const Botkit = require('botkit')
 // const locationDialog = require('botbuilder-location')
-const controller = Botkit.botframeworkbot({})
+
+const port = process.env.PORT || 8888
+const authorizationHeader = process.env.BRAIN_JWT || 'pizza'
+
+// express configuration
+const app = express()
+// public folder for static content
+app.use(express.static(path.join(__dirname, '/public')))
+// parsing application/json and urlencoded
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+// the http server
+const server = http.createServer(app)
+
+// this is a flag that reflects the status of the connection with the brain
+let brainConnected = false
+let websocket = null
+
+// configuration of the websocket for communication with the brain
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', (ws, request) => {
+  // TODO check the path inside resource
+  console.log('request received')
+  const urlParsed = url.parse(request.url, true)
+  if (urlParsed.pathname !== '/brain') {
+    ws.close(3001, 'please connect on the path /brain')
+    return
+  }
+  if (urlParsed.query['jwt'] !== authorizationHeader) {
+    ws.close(3002, 'authentication of brain failed')
+    return
+  }
+  if (brainConnected) {
+    // the brain is already connected
+    ws.close(3003, 'a brain is already connected')
+    console.log('the brain is already connected. Rejected connection request ' + request)
+  } else {
+    // TODO check authentication of brain (some headers constant)
+    console.log('accepting brain connection request' + request)
+
+    brainConnected = true
+    websocket = ws
+
+    ws.on('message', (message) => {
+      // TODO message from brain received
+      console.log('Received Message from brain: ' + message)
+      ws.send('ok received ' + message)
+    })
+
+    ws.on('close', (message) => {
+      // closed connection with the brain
+      brainConnected = false
+      websocket = null
+    })
+  }
+})
+
+// configuration of botkit
+const controller = Botkit.botframeworkbot({ port: port })
 
 const bot = controller.spawn({
   appId: process.env.app_id,
   appPassword: process.env.app_password
 })
 
-// this is a flag that reflects the status of the connection with the brain
-let brainConnected = false
-
-// TODO configuration of the websocket for communication with the brain
-
 // configuration of the the webser endpoints
-controller.setupWebserver(process.env.PORT, (err, webserver) => {
-  if (err) {
-    throw err
-  }
-  controller.createWebhookEndpoints(controller.webserver, bot, function () {
-    console.log('This bot is online!!!')
-  })
+controller.createWebhookEndpoints(app, bot, function () {
+  console.log('This bot is online!!!')
 })
 
+// message received from botframework
 controller.on('message_received', (bot, message) => {
   console.log(message)
   // TODO preprocess message
   // TODO check if brain connected
   if (brainConnected) {
     // deliver the message to the brain
+    websocket.send(message)
     // TODO
     bot.reply(message, 'sent to the brain')
   } else {
@@ -36,47 +93,7 @@ controller.on('message_received', (bot, message) => {
   }
 })
 
-/*
-// user said hello
-controller.hears(['hello'], 'message_received', function(bot, message) {
-
-    bot.reply(message, 'Hey there.');
-
-});
-
-controller.hears(['location'], 'message_received', function(bot, message) {
-    bot.reply(message, {
-        attachments: [{
-            contentType: 'application/vnd.microsoft.card.hero',
-            content: {
-                title: "I'm a hero card",
-                subtitle: "Pig Latin Wikipedia Page",
-                images: [{
-                        url: "https://<ImageUrl1>"
-                    },
-                    {
-                        url: "https://<ImageUrl2>"
-                    }
-                ],
-                buttons: [{
-                    type: "openUrl",
-                    title: "WikiPedia Page",
-                    value: "https://en.wikipedia.org/wiki/Pig_Latin"
-                }]
-            }
-        }]
-    })
+// finally start listening on the port
+server.listen(port, () => {
+  console.log('Listening on %d', server.address().port)
 })
-
-controller.hears(['cookies'], 'message_received', function(bot, message) {
-
-    bot.startConversation(message, function(err, convo) {
-
-        convo.say('Did someone say cookies!?!!');
-        convo.ask('What is your favorite type of cookie?', function(response, convo) {
-            convo.say('Golly, I love ' + response.text + ' too!!!');
-            convo.next();
-        });
-    });
-});
-*/

baf6224d95d58c68b6a78cc5392a753fcd36f18b
Martino Mensio <martinomensio@outlook.it>
2018-01-05 15:11:37 +0100

    removed duplicated message and changed convo object generation

--- app.js
+++ app.js
@@ -37,16 +37,22 @@ const onWebsocketMessage = (message, language) => {
     if (context) {
       // send via botkit
       const bot = bots.bots[language]
-      bot.startConversation(context, (err, convo) => {
-        if (err) {
-          throw err
-        }
-        switch (message.type) {
-          case 'text':
+      switch (message.type) {
+        case 'text':
+          bot.startConversation(context, (err, convo) => {
+            if (err) {
+              throw err
+            }
             convo.say(message.text)
-            break
-          // request_location
-          case 'request_location':
+            convo.next()
+          })
+          break
+        // request_location
+        case 'request_location':
+          bot.startConversation(context, (err, convo) => {
+            if (err) {
+              throw err
+            }
             // ask the user for their location, using native channel data
             if (message.userId.startsWith('facebook')) {
               const fbChannelData = {
@@ -81,72 +87,78 @@ const onWebsocketMessage = (message, language) => {
             } else {
               convo.say(message.text)
             }
-            break
-          // map for the location
-          case 'map':
-            const buttons = (message.buttons || []).map(el => {
-              switch (el.type) {
-                // a button that on click sends a message with the value
-                case 'text':
-                  return {
-                    type: 'imBack',
-                    title: el.value,
-                    value: el.value
-                  }
-                // a button that on click opens a link in the browser
-                case 'link':
-                  return {
-                    type: 'openUrl',
-                    title: el.title || el.value,
-                    value: el.value
-                  }
-              }
-            })
-            convo.say(message.text)
-            // points is an array of objects like {'type': 'from'/'to'/'bike'/'slot', 'value': [lat,lng]}
-            const points = Object.assign(...message.markers.map(d => ({ [d['type']]: d['value'] })))
-            if (points.slot) {
-              buttons.unshift({
-                type: 'openUrl',
-                title: points.slot.name,
-                value: `https://www.google.com/maps/place/${points.slot.lat},${points.slot.lng}`
-              })
-            }
-            if (points.bike) {
-              buttons.unshift({
-                type: 'openUrl',
-                title: points.bike.name,
-                value: `https://www.google.com/maps/place/${points.bike.lat},${points.bike.lng}`
-              })
-            }
-            mapManager.pointsToMapUrl(points).then(url => {
-              console.log('received URL for map')
-              bot.startConversation(context, (err, convo) => {
-                if (err) {
-                  throw err
+            convo.next()
+          })
+          break
+        // map for the location
+        case 'map':
+          const buttons = (message.buttons || []).map(el => {
+            switch (el.type) {
+              // a button that on click sends a message with the value
+              case 'text':
+                return {
+                  type: 'imBack',
+                  title: el.value,
+                  value: el.value
                 }
-                convo.say({
-                  // text: message.text,
-                  attachments: [
-                    {
-                      contentType: 'application/vnd.microsoft.card.hero',
-                      content: {
-                        // title: 'Results',
-                        text: message.text,
-                        images: [{
-                          'url': url,
-                          alt: 'location'
-                        }],
-                        buttons: buttons
-                      }
-                    }
-                  ]
-                })
-              })
+              // a button that on click opens a link in the browser
+              case 'link':
+                return {
+                  type: 'openUrl',
+                  title: el.title || el.value,
+                  value: el.value
+                }
+            }
+          })
+          // points is an array of objects like {'type': 'from'/'to'/'bike'/'slot', 'value': [lat,lng]}
+          const points = Object.assign(...message.markers.map(d => ({ [d['type']]: d['value'] })))
+          if (points.slot) {
+            buttons.unshift({
+              type: 'openUrl',
+              title: points.slot.name,
+              value: `https://www.google.com/maps/place/${points.slot.lat},${points.slot.lng}`
             })
-            break
-          // display a button to the user asking facebook login
-          case 'login':
+          }
+          if (points.bike) {
+            buttons.unshift({
+              type: 'openUrl',
+              title: points.bike.name,
+              value: `https://www.google.com/maps/place/${points.bike.lat},${points.bike.lng}`
+            })
+          }
+          mapManager.pointsToMapUrl(points).then(url => {
+            console.log('received URL for map')
+            bot.startConversation(context, (err, convo) => {
+              if (err) {
+                throw err
+              }
+              convo.say({
+                // text: message.text,
+                attachments: [
+                  {
+                    contentType: 'application/vnd.microsoft.card.hero',
+                    content: {
+                      // title: 'Results',
+                      text: message.text,
+                      images: [{
+                        'url': url,
+                        alt: 'location'
+                      }],
+                      buttons: buttons
+                    }
+                  }
+                ]
+              })
+              convo.next()
+            })
+          })
+          break
+        // display a button to the user asking facebook login
+        case 'login':
+          bot.startConversation(context, (err, convo) => {
+            if (err) {
+              throw err
+            }
             convo.say({
               attachments: [{
                 contentType: 'application/vnd.microsoft.card.signin',
@@ -160,13 +172,12 @@ const onWebsocketMessage = (message, language) => {
                 }
               }]
             })
-            break
-          default:
-            break
-        }
-        // TODO
-        convo.next()
-      })
+            convo.next()
+          })
+          break
+        default:
+          break
+      }
     } else {
       // the context does not exist
       throw new Error('no context for this chat. Maybe this user never sent a message to the bot?')
